2596083e6f166e8159903542d87a95307c78cd69
mehmetbuber <mehmetbuber@gmail.com>
2019-02-23 03:39:11 +0300

    Save

--- src/extension/code/js.js
+++ src/extension/code/js.js
@@ -1,107 +1,79 @@
-var hidden = 0;
 var interval;
-var clearCount = 0;
 var intervalState = false;
+var hrefs = [];
+var visitedThreads = [];
 
 document.onkeyup = function (e) {
     var location = window.location.href;
     if (location.indexOf("https://www.reddit.com/") !== -1 && location.indexOf("/comments/") === -1) {
         if (e.ctrlKey && e.shiftKey && e.which === 32) {
-            clearCount = 0;
             getUrls(function (data) {
-                var hrefs = [];
-
-                if (!data.visitedThreads) {
-                    data.visitedThreads = [];
-                }
-
-                $(".scrollerItem").each(function () {
-                    $(this).find("a").each(function () {
-                        var href = $(this).attr("href");
-
-                        if (href.indexOf("https://www.reddit.com/") === -1) {
-                            if (href.indexOf("?instanceId=") === -1) {
-                                if (href.indexOf("/comments/") !== -1) {
-                                    href = "https://www.reddit.com" + href;
-                                    if (data.visitedThreads.indexOf(href) === -1 && hrefs.indexOf(href) === -1) {
-                                        if (hrefs.length < 5) {
-                                            hrefs.push(href);
+                interval = setInterval(function () {
+                    if (!intervalState) {
+                        $(window).scrollTop(0);
+                        $(".scrollerItem").each(function () {
+                            var ths = $(this);
+                            $(this).find("a").each(function () {
+                                var href = $(this).attr("href");
+                                if (href.indexOf("https://www.reddit.com/") === -1) {
+                                    if (href.indexOf("?instanceId=") === -1) {
+                                        if (href.indexOf("/comments/") !== -1) {
+                                            href = "https://www.reddit.com" + href;
+                                            if (data.visitedThreads.indexOf(href) === -1) {
+                                                if (hrefs.indexOf(href) === -1) {
+                                                    if (hrefs.length < 5) {
+                                                        hrefs.push(href);
+                                                        ths.remove();
+                                                    }
+                                                } else {
+                                                    ths.remove();
+                                                }
+                                            } else {
+                                                ths.remove();
+                                            }
                                         }
+                                    } else {
+                                        ths.remove();
                                     }
                                 }
-                            } 
-                        }
-                    });
-                });
-
-                for (var i = 0; i < hrefs.length; i++) {
-                    window.open(hrefs[i], "_blank");
-                }
-
-                addUrls(hrefs, function() {
-                    interval = setInterval(function () {
-                        if (!intervalState) {
-                            $(window).scrollTop(0);
-
-                            if (hidden === 0) {
-                                clearCount++;
-                            } else {
-                                clearCount = 0;
-                            }
-
-                            if (clearCount > 0) {
-                                intervalState = true;
-                            }
-
-                            if (clearCount > 30 || hrefs.length === 5) {
-                                clearInterval(interval);
-                            }
-
-                            hidden = 0;
-
-                            getUrls(function (result) {
-                                $(".scrollerItem").each(function () {
-                                    var ths = $(this);
-                                    $(this).find("a").each(function () {
-                                        var href = $(this).attr("href");
-                                        if (href.indexOf("?instanceId=") !== -1) {
-                                            ths.remove();
-                                            hidden++;
-                                        }
-
-                                        if (result.visitedThreads.indexOf(href) !== -1) {
-                                            ths.remove();
-                                            hidden++;
-                                        }
-                                    });
-                                });
                             });
-                        } else {
-                            $(window).scrollTop(clearCount * 300);
-                            intervalState = false;
+                        });
+
+                        console.log(hrefs);
+
+                        if (hrefs.length === 5)
+                            finalize();
+                        else {
+                            intervalState = true;
                         }
-                    }, 100);
-                });
+
+                    } else {
+                        $(window).scrollTop(1200);
+                        intervalState = false;
+                    }
+                }, 100);
+
             });
         }
     }
 };
 
-function addUrls(urls, callback) {
-    getUrls(function (data) {
-        if (!data.visitedThreads)
-            data.visitedThreads = [];
-
-        data.visitedThreads = data.visitedThreads.concat(urls);
-
-        chrome.storage.local.set({ visitedThreads: data.visitedThreads }, function (result) {
-            callback(result);
-        });
-    });
-}
-
 function getUrls(callback) {
     chrome.storage.local.get(["visitedThreads"], function (result) {
         callback(result);
+    });
+}
+
+function finalize() {
+    clearInterval(interval);
+    for (var i = 0; i < hrefs.length; i++) {
+        window.open(hrefs[i], "_blank");
+    }
+
+    visitedThreads = visitedThreads.concat(hrefs);
+    hrefs = [];
+
+    chrome.storage.local.set({ visitedThreads: visitedThreads }, function (result) {
+        console.log(result);
     });
 }
--- src/extension/code/popup.js
+++
@@ -1,9 +0,0 @@
-﻿var manif = chrome.runtime.getManifest();
-
-$(document).ready(function () {
-    chrome.storage.local.get(function (store) {
-        chrome.tabs.getSelected(null, function (tab) {
-            var tablink = tab.url;
-        });
-    });
-});
