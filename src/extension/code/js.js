978f69f5c8ca540d5b6dac35c1e8ea16cbc14a60
mehmetbuber <mehmetbuber@gmail.com>
2019-02-23 06:34:42 +0300

    Loading gif

--- src/extension/code/js.js
+++ src/extension/code/js.js
@@ -3,7 +3,7 @@ var intervalState = false;
 var hrefs = [];
 var codes = [];
 var visitedThreads = [];
-var linkCount = 50;
+var linkCount = 5;
 var subReddit = "";
 var keys = [];
 var i = 0;
@@ -11,81 +11,87 @@ var pageLocation = "";
 var ths;
 var href = "";
 var id = "";
+var working = false;
 
 document.onkeyup = function (e) {
+    if (!working) {
+        chrome.storage.local.get(function (val) {
+            console.log(val);
+        });
 
-    chrome.storage.local.get(function (val) {
-        console.log(val);
-    });
+        pageLocation = window.location.href;
+        subReddit = pageLocation.replace("https://www.reddit.com", "").replace("/r/", "").split("?")[0].split("/")[0];
 
-    pageLocation = window.location.href;
-    subReddit = pageLocation.replace("https://www.reddit.com", "").replace("/r/", "").split("?")[0].split("/")[0];
+        if (!subReddit)
+            subReddit = "reddit";
 
-    if (!subReddit)
-        subReddit = "reddit";
+        if (subReddit.length === 0)
+            subReddit = "reddit";
 
-    if (subReddit.length === 0)
-        subReddit = "reddit";
+        if (e.ctrlKey && e.shiftKey && e.which === 32) {
+            working = true;
+            $("body").prepend("<div id=\"reddit-no-repeat-loading\">" +
+                "<img class=\"reddit-loading\" src=\"http://mehmetbuber.com/loading.gif\">" +
+                "<p>Loading new threads, please wait...</p>" +
+                "</div>");
 
-    if (e.ctrlKey && e.shiftKey && e.which === 32) {
-        if (pageLocation.indexOf("/comments/") === -1) {
-            getUrls(function (data) {
-                visitedThreads = data[subReddit];
+            if (pageLocation.indexOf("/comments/") === -1) {
+                getUrls(function (data) {
+                    visitedThreads = data[subReddit];
 
-                if (subReddit === "reddit") {
-                    keys = Object.keys(data);
-                    for (i = 0; i < keys.length; i++) {
-                        if (keys[i] !== "linkCount") {
-                            if (Array.isArray(data[keys[i]])) {
-                                visitedThreads = visitedThreads.concat(data[keys[i]]);
+                    if (subReddit === "reddit") {
+                        keys = Object.keys(data);
+                        for (i = 0; i < keys.length; i++) {
+                            if (keys[i] !== "linkCount") {
+                                if (Array.isArray(data[keys[i]])) {
+                                    visitedThreads = visitedThreads.concat(data[keys[i]]);
+                                }
                             }
                         }
                     }
-                }
-                
-                if (!visitedThreads)
-                    visitedThreads = [];
 
-                interval = setInterval(function () {
-                    $(window).scrollTop(0);
-                    console.log($(".scrollerItem").length);
-                    $(".scrollerItem").each(function () {
-                        ths = $(this);
-                        $(this).find("a").each(function () {
-                            href = $(this).attr("href");
-                            if (href.indexOf("https://www.reddit.com/") === -1) {
-                                if (href.indexOf("?instanceId=") === -1) {
-                                    if (href.indexOf("/comments/") !== -1) {
-                                        id = getId(href);
-                                        if (visitedThreads.indexOf(id) === -1) {
-                                            if (hrefs.indexOf(href) === -1) {
-                                                if (hrefs.length < linkCount) {
-                                                    codes.push(id);
-                                                    hrefs.push(href);
-                                                    ths.remove();
+                    if (!visitedThreads)
+                        visitedThreads = [];
+
+                    interval = setInterval(function () {
+                        $(window).scrollTop(0);
+                        $(".scrollerItem").each(function () {
+                            ths = $(this);
+                            $(this).find("a").each(function () {
+                                href = $(this).attr("href");
+                                if (href.indexOf("https://www.reddit.com/") === -1) {
+                                    if (href.indexOf("?instanceId=") === -1) {
+                                        if (href.indexOf("/comments/") !== -1) {
+                                            id = getId(href);
+                                            if (visitedThreads.indexOf(id) === -1) {
+                                                if (hrefs.indexOf(href) === -1) {
+                                                    if (hrefs.length < linkCount) {
+                                                        codes.push(id);
+                                                        hrefs.push(href);
+                                                        ths.parent().parent().remove();
+                                                    }
+                                                } else {
+                                                    ths.parent().parent().remove();
                                                 }
                                             } else {
-                                                ths.remove();
+                                                ths.parent().parent().remove();
                                             }
-                                        } else {
-                                            ths.remove();
                                         }
+                                    } else {
+                                        ths.parent().parent().remove();
                                     }
-                                } else {
-                                    ths.remove();
                                 }
-                            }
+                            });
                         });
-                    });
 
-                    if (hrefs.length > linkCount - 1)
-                        finalize();
-                    else {
-                        $(window).scrollTop(120000);
-                        //intervalState = true;
-                    }
-                }, 200);
-            });
+                        if (hrefs.length > linkCount - 1)
+                            finalize();
+                        else {
+                            window.scrollTo(0, document.body.scrollHeight);
+                        }
+                    }, 200);
+                });
+            }
         }
     }
 };
@@ -106,6 +112,7 @@ function getUrls(callback) {
 }
 
 function finalize() {
+    document.documentElement.scrollTop = 0;
     clearInterval(interval);
     for (i = 0; i < hrefs.length; i++) {
         window.open("https://www.reddit.com" + hrefs[i], "_blank");
@@ -124,5 +131,8 @@ function finalize() {
         visitedThreads = [];
         hrefs = [];
         codes = [];
+
+        $("#reddit-no-repeat-loading").remove();
+        working = false;
     });
 }
