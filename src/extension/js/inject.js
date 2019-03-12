6ee6f1592251c6db230f7bdb1995e97de02d665f
mehmetbuber <mehmetbuber@gmail.com>
2019-03-12 07:26:17 +0300

    Save

--- src/extension/js/inject.js
+++ src/extension/js/inject.js
@@ -1,212 +1,195 @@
-
-$(function () {
-});
-
 document.onclick = function (event) {
     console.log(event);
-    if (!event.ctrlKey)
+    if (!event.ctrlKey || !event.shiftKey)
         return;
 
     var target = 'target' in event ? event.target : event.srcElement;
 
-    var root = document.compatMode === 'CSS1Compat' ? document.documentElement : document.body;
-    var mxy = [event.clientX + root.scrollLeft, event.clientY + root.scrollTop];
-
-    var path = getPathTo(target);
     var txy = getPageXY(target);
 
-    var message = 'You clicked the element ' + path + ' at offset ' + (mxy[0] - txy[0]) + ', ' + (mxy[1] - txy[1]);
     var item = {
-        path: path,
-        offsetX: (mxy[0] - txy[0]),
-        offsetY: (mxy[1] - txy[1]),
-        url: location.protocol + '//' + location.host + location.pathname
+        id: generateId(),
+        x: txy[0] / window.innerWidth,
+        y: txy[1] / window.innerHeight,
+        query: window.location.search
     };
 
     console.log(item);
-    console.log(message);
-    var elem = getElementByXpath(path);
 
     var note = prompt("Please enter your note", "");
     item.note = note;
 
     if (note !== null && note !== "") {
-        $(elem).append('<div class="quote-container">' +
-            '<i class="pin"></i>' +
-            '<blockquote class="note yellow">' +
-            '' + note + '<cite class="author">Albert Einstein</cite>' +
-            '' + note + '<cite class="author">Albert Einstein</cite>' +
-            '' + note + '<cite class="author">Albert Einstein</cite>' +
-            '</blockquote>' +
-            '</div>');
-        $(elem).append(
-            '<div class="row message" id="conversation"><div class="row message-previous"><div class="col-sm-12 previous">' +
-            '<a onclick="previous(this)" id="ankitjain28" name="20">Show Previous Message!</a></div></div><div class="row message-body">' +
-            '<div class="col-sm-12 message-main-receiver"><div class="receiver"><div class="message-text">Hyy, Its Awesome..!</div>' +
-            '<span class="message-time pull-right">Sun</span></div></div></div><div class="row message-body"><div class="col-sm-12 message-main-sender">' +
-            '<div class="sender"><div class="message-text">Thanks n I know its awesome...!</div><span class="message-time pull-right">Sun</span>' +
-            '</div></div></div></div><!-- Message Box End --><!-- Reply Box --><div class="row reply"><div class="col-sm-1 col-xs-1 reply-emojis">' +
-            '<i class="fa fa-smile-o fa-2x"></i></div><div class="col-sm-9 col-xs-9 reply-main"><textarea class="form-control" rows="1" id="comment"></textarea>' +
-            '</div><div class="col-sm-1 col-xs-1 reply-recording"><i class="fa fa-microphone fa-2x" aria-hidden="true"></i></div><div class="col-sm-1 col-xs-1 reply-send">' +
-            '<i class="fa fa-send fa-2x" aria-hidden="true"></i></div></div>');
+        chrome.storage.local.get(function (data) {
+            var host = window.location.host.replace(/[^\w\s]/gi, '');
+            var path = window.location.pathname.replace(/[^\w\s]/gi, '');
 
-        chrome.storage.local.get(function (result) {
-            if (!result)
-                result = {};
+            if (!data.urls)
+                data.urls = [];
 
-            if (!result["items"])
-                result["items"] = [];
+            if (!data["pages"])
+                data["pages"] = {};
 
-            result["items"].push(item);
-            console.log(result["items"]);
-            chrome.storage.local.set(result);
+            if (!data["pages"][host])
+                data["pages"][host] = {};
+
+            if (!data["pages"][host][path])
+                data["pages"][host][path] = [];
+
+            data["pages"][host][path].push(item);
+
+            console.log(data);
+            $('body').append(' <div class="chat"> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <div class="chat-container"> <div class="user-bar"> <div class="back"> <i class="zmdi zmdi-arrow-left"></i> </div><div class="checkbox"> <label style="font-size: 1em"> <input type="checkbox" value=""> <span class="cr"><i class="cr-icon fa fa-check"></i></span> </label> </div><div class="name"> <span>' + note + '</span> <span class="note-status">pending</span> </div><div class="actions"> <i class="fa fa-eye-slash showButton"></i> <i class="fa fa-close closeButton"></i> </div></div><div class="conversation" style="display: none"> <div class="conversation-container"> <div class="message sent"> What happened last night? <br/> <hr style="margin: 0; margin-top: 5px"/> <span class="metadata"> Mehmet Buber<br/> <span class="time">17/02/2018 22:03</span> </span> </div><div class="message received"> You were drunk. <br/> <hr style="margin: 0; margin-top: 5px"/> <span class="metadata-received"> Mehmet Buber<br/> <span class="time">17/02/2018 22:03</span> </span> </div><div class="message sent"> No I wasnt. <br/> <hr style="margin: 0; margin-top: 5px"/> <span class="metadata"> Mehmet Buber<br/> <span class="time">17/02/2018 22:03</span> </span> </div><div class="message received"> <span id="random">You were hugging an old man with a beard screaming "DUMBLEDORE YOURE ALIVE!"</span> <br/> <hr style="margin: 0; margin-top: 5px"/> <span class="metadata-received"> Mehmet Buber<br/> <span class="time">17/02/2018 22:03</span> </span> </div></div><form class="conversation-compose"> <input class="input-msg" name="input" placeholder="Type a message" autocomplete="off" autofocus></input> <div class="send"> <div class="circle"> <i class="zmdi zmdi-mail-send"></i> </div></div></form> </div></div></div>');
+
+            setupDraggable();
+            chrome.storage.local.set(data);
         });
-    } 
+    }
 };
 
 $(document).ready(function () {
-    chrome.storage.local.get(function (result) {
-        if (!result)
-            result = {};
+    chrome.storage.local.get(function (data) {
+        var host = window.location.host.replace(/[^\w\s]/gi, '');
+        var path = window.location.pathname.replace(/[^\w\s]/gi, '');
 
-        if (!result["items"])
-            result["items"] = [];
+        if (!data.urls)
+            data.urls = [];
 
-        for (var i = 0; i < result["items"].length; i++) {
-            console.log(result["items"][i]);
-            var elem = getElementByXpath(result["items"][i].path);
-            $(elem).append('' +
-                '<div class="quote-container">' +
-                '<input class="todo-check" type="checkbox"><hr>' +
-                '<div class="message " id="conversation">' +
-                '<div class="row message-previous">' +
-                '<div class="col-sm-12 previous">' +
-                '<a onclick="previous(this)" id="ankitjain28" name="20">Show Previous Message!</a>' +
+        if (!data["pages"])
+            data["pages"] = {};
+
+        if (!data["pages"][host])
+            data["pages"][host] = {};
+
+        if (!data["pages"][host][path])
+            data["pages"][host][path] = [];
+
+        var notes = $('body').prepend('<div id="path-notes" style="position: relative"></div>');
+        for (var i = 0; i < data["pages"][host][path].length; i++) {
+            $('#path-notes').append('' +
+                '<div class="chat" data-id="' + data["pages"][host][path][i].id + '" data-host="' + host + '" data-path="' + path + '" style="left: ' + (data["pages"][host][path][i].x * window.innerWidth) + 'px; top: ' + (data["pages"][host][path][i].y * window.innerHeight) + 'px; position: absolute">' +
+                '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">' +
+                ' <div class="chat-container"> ' +
+                '<div class="user-bar"> ' +
+                '<div class="back"> ' +
+                '<i class="zmdi zmdi-arrow-left"></i> ' +
                 '</div>' +
+                '<div class="checkbox"> ' +
+                '<label style="font-size: 1em"> ' +
+                '<input type="checkbox" value="">' +
+                ' <span class="cr">' +
+                '<i class="cr-icon fa fa-check"></i>' +
+                '</span> ' +
+                '</label> ' +
                 '</div>' +
-                '<div class="row message-body">' +
-                '<div class="col-sm-12 message-main-receiver">' +
-                '<div class="receiver">' +
-                '<div class="message-text">' + result["items"][i].note + '</div>' +
-                '<span class="message-time pull-right">Sun</span>' +
+                '<div class="name"> ' +
+                '<span>' + data["pages"][host][path][i].note + '</span> ' +
+                '<span class="note-status">pending</span>' +
+                ' </div><div class="actions">' +
+                ' <i class="fa fa-eye-slash showButton"></i>' +
+                ' <i class="fa fa-close closeButton"></i>' +
+                ' </div>' +
                 '</div>' +
+                '<div class="conversation" style="display: none">' +
+                ' <div class="conversation-container">' +
+                ' <div class="message sent"> What happened last night? <br/>' +
+                ' <hr style="margin: 0; margin-top: 5px"/>' +
+                ' <span class="metadata"> Mehmet Buber<br/> ' +
+                '<span class="time">17/02/2018 22:03</span>' +
+                ' </span> ' +
                 '</div>' +
-                '</div>' +
-                '<div class="row message-body">' +
-                '<div class="col-sm-12 message-main-sender">' +
-                '<div class="sender">' +
-                '<div class="message-text">Thanks n I know its awesome...!</div>' +
-                '<span class="message-time pull-right">Sun</span>' +
-                '</div>' +
-                '</div>' +
-                '</div>' +
-                '</div>' +
-                '<!-- Message Box End --><!-- Reply Box -->' +
-                '<div class="row reply">' +
-                '<div class="col-sm-9 col-xs-9 reply-main">' +
-                '<textarea class="form-control" rows="1" id="comment"></textarea>' +
-                '</div>' +
-                '<div class="col-sm-1 col-xs-1 reply-recording">' +
-                '<i class="fa fa-microphone fa-2x" aria-hidden="true"></i>' +
-                '</div>' +
-                '<div class="col-sm-1 col-xs-1 reply-send">' +
-                '<i class="fa fa-send fa-2x" aria-hidden="true"></i>' +
-                '</div>' +
-                '</div>' +
-                '</div>');
+                '<div class="message received"> You were drunk. <br/> ' +
+                '<hr style="margin: 0; margin-top: 5px"/> ' +
+                '<span class="metadata-received"> Mehmet Buber<br/>' +
+                ' <span class="time">17/02/2018 22:03</span> ' +
+                '</span>' +
+                ' </div>' +
+                '<div class="message sent"> No I wasnt. <br/> <hr style="margin: 0; margin-top: 5px"/> <span class="metadata"> Mehmet Buber<br/>' +
+                ' <span class="time">17/02/2018 22:03</span> </span> </div><div class="message received">' +
+                ' <span id="random">You were hugging an old man with a beard screaming "DUMBLEDORE YOURE ALIVE!"</span>' +
+                ' <br/> <hr style="margin: 0; margin-top: 5px"/> <span class="metadata-received"> Mehmet Buber<br/> ' +
+                '<span class="time">17/02/2018 22:03</span> </span> </div></div><form class="conversation-compose"> <input class="input-msg" name="input" placeholder="Type a message" autocomplete="off" autofocus></input> <div class="send"> <div class="circle"> <i class="zmdi zmdi-mail-send"></i> </div></div></form> </div></div></div>');
         }
 
-        $(".quote-container").each(function() {
-            $(this).draggable();
-        });
+        setupDraggable();
     });
 });
+
 function getElementByXpath(path) {
     return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
+function setupDraggable() {
+    $(".chat").each(function () {
+        var ths = $(this);
+        ths.draggable({
+            snap: true,
+            scroll: false,
+            handle: ".user-bar",
+            stop: function (e) {
+                chrome.storage.local.get(function (data) {
+                    var id = ths.attr("data-id");
+                    var host = ths.attr("data-host");
+                    var path = ths.attr("data-path");
+                    
+                    if (!data.urls)
+                        data.urls = [];
 
+                    if (!data["pages"])
+                        data["pages"] = {};
 
-//*[@id='answer-2631931']/div[1]/div[2]/div[1]/p[1]
-function getPathTo(element) {
-    if (element.id!=='')
-        return "//*[@id='"+element.id+"']";
-    
-    if (element===document.body)
-        return element.tagName.toLowerCase();
+                    if (!data["pages"][host])
+                        data["pages"][host] = {};
 
-    var ix= 0;
-    var siblings= element.parentNode.childNodes;
-    for (var i= 0; i<siblings.length; i++) {
-        var sibling= siblings[i];
-        
-        if (sibling===element) return getPathTo(element.parentNode) + '/' + element.tagName.toLowerCase() + '[' + (ix + 1) + ']';
-        
-        if (sibling.nodeType===1 && sibling.tagName === element.tagName) {
-            ix++;
-        }
-    }
+                    if (!data["pages"][host][path])
+                        data["pages"][host][path] = [];
+
+                    var newX = parseInt(ths.css('left').replace("px", "")) / window.innerWidth;
+                    var newY = parseInt(ths.css('top').replace("px", "")) / window.innerHeight;
+
+                    console.log(parseInt(ths.css('left').replace("px", "")));
+                    console.log(parseInt(ths.css('top').replace("px", "")));
+
+                    console.log(newX * window.innerWidth);
+                    console.log(newY * window.innerHeight);
+
+                    data["pages"][host][path].forEach(function (obj) {
+                        if (obj.id === id) {
+                            obj.x = newX;
+                            obj.y = newY;
+                        }
+                    });
+                    chrome.storage.local.set(data);
+                });
+            }
+        });
+
+    });
+
 }
+$(document).on('click', 'body .hideButton', function () {
+    $(this).closest('.chat').find('.conversation').hide();
+    $(this).removeClass("hideButton");
+    $(this).addClass("showButton");
+    $(this).removeClass("fa-eye");
+    $(this).addClass("fa-eye-slash");
+});
+
+$(document).on('click', 'body .showButton', function () {
+    $(this).closest('.chat').find('.conversation').show();
+    $(this).removeClass("showButton");
+    $(this).addClass("hideButton");
+    $(this).addClass("fa-eye");
+    $(this).removeClass("fa-eye-slash");
+});
 
 function getPageXY(element) {
-    var x= 0, y= 0;
+    var x = 0, y = 0;
     while (element) {
-        x+= element.offsetLeft;
-        y+= element.offsetTop;
-        element= element.offsetParent;
+        x += element.offsetLeft;
+        y += element.offsetTop;
+        element = element.offsetParent;
     }
     return [x, y];
-}
-
-/* Meme */
-
-var memes = [
-    'Dude, you smashed my turtle saying "I\'M MARIO BROS!"',
-    'Dude, you grabed seven oranges and yelled "I GOT THE DRAGON BALLS!"',
-    'Dude, you threw my hamster across the room and said "PIKACHU I CHOOSE YOU!"',
-    'Dude, you congratulated a potato for getting a part in Toy Story',
-    'Dude, you were hugging an old man with a beard screaming "DUMBLEDORE YOU\'RE ALIVE!"',
-    'Dude, you were cutting all my pinapples yelling "SPONGEBOB! I KNOW YOU\'RE THERE!"',
-];
-
-var random = document.querySelector('#random');
-
-random.innerHTML = memes[Math.floor(Math.random() * memes.length)];
-
-/* Time */
-
-var deviceTime = document.querySelector('.status-bar .time');
-var messageTime = document.querySelectorAll('.message .time');
-
-deviceTime.innerHTML = moment().format('h:mm');
-
-setInterval(function () {
-    deviceTime.innerHTML = moment().format('h:mm');
-}, 1000);
-
-for (var i = 0; i < messageTime.length; i++) {
-    messageTime[i].innerHTML = moment().format('h:mm A');
-}
-
-/* Message */
-
-var form = document.querySelector('.conversation-compose');
-var conversation = document.querySelector('.conversation-container');
-
-form.addEventListener('submit', newMessage);
-
-function newMessage(e) {
-    var input = e.target.input;
-
-    if (input.value) {
-        var message = buildMessage(input.value);
-        conversation.appendChild(message);
-        animateMessage(message);
-    }
-
-    input.value = '';
-    conversation.scrollTop = conversation.scrollHeight;
-
-    e.preventDefault();
 }
 
 function buildMessage(text) {
@@ -226,9 +209,12 @@ function buildMessage(text) {
     return element;
 }
 
-function animateMessage(message) {
-    setTimeout(function () {
-        var tick = message.querySelector('.tick');
-        tick.classList.remove('tick-animation');
-    }, 500);
+function generateId() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    for (var i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
 }
